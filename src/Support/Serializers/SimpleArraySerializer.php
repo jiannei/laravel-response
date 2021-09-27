ba90958a6bdcfbd98807bb1d98b7d4a2752fa3df
Jiannei <longjian.huang@foxmail.com>
2021-09-27 17:45:27 +0800

    feat: simple ArraySerializer

---
+++ src/Support/Serializers/SimpleArraySerializer.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Jiannei/laravel-response.
+ *
+ * (c) Jiannei <longjian.huang@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Jiannei\Response\Laravel\Support\Serializers;
+
+class SimpleArraySerializer extends ArraySerializer
+{
+    /**
+     * Serialize a collection.
+     *
+     * @param  string  $resourceKey
+     * @param  array  $data
+     * @return array
+     */
+    public function collection($resourceKey, array $data)
+    {
+        return $data;
+    }
+}
