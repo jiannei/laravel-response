fbb6cb3401ea90c86c067d55b4f0494b2be056d3
jiannei <longjian.huang@foxmail.com>
2024-03-13 14:27:13 +0800

    feat: support laravel11

---
+++ src/Http/Exceptions/Handler.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Jiannei\Response\Laravel\Http\Exceptions;
+
+use Jiannei\Response\Laravel\Support\Traits\ExceptionTrait;
+
+class Handler extends \Illuminate\Foundation\Exceptions\Handler
+{
+    use ExceptionTrait;
+}
--- src/Providers/LaravelServiceProvider.php
+++ src/Providers/LaravelServiceProvider.php
@@ -13,6 +13,7 @@ namespace Jiannei\Response\Laravel\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Jiannei\Response\Laravel\Contract\ResponseFormat;
+use Jiannei\Response\Laravel\Http\Exceptions\Handler;
 use Jiannei\Response\Laravel\Support\Format;
 
 class LaravelServiceProvider extends ServiceProvider
@@ -20,6 +21,11 @@ class LaravelServiceProvider extends ServiceProvider
     public function register()
     {
         $this->setupConfig();
+
+        $this->app->singleton(
+            \Illuminate\Contracts\Debug\ExceptionHandler::class,
+            Handler::class
+        );
 
         $this->app->singleton(ResponseFormat::class, function ($app) {
             $formatter = $app->config->get('response.format.class');
